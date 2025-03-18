799625a8b866d5254f2cbf0ca584d53632ca1688
ErFaN <safarierfan29@gmail.com>
2025-03-18 23:14:03 +0330

    upload

--- app/models/User.js
+++ app/models/User.js
@@ -1,6 +1,5 @@
 export default class User {
     getAllUsers() {
-        // این یک مثال ساده است - در واقعیت معمولاً از دیتابیس استفاده می‌شود
         return [
             { id: 1, name: 'علی', email: 'ali@example.com' },
             { id: 2, name: 'مریم', email: 'maryam@example.com' },
